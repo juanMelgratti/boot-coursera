17d5c7145affb864d070728426269a6c779d76a8
juanMelgratti <juanmelgratti@mi.unc.edu.ar>
2020-09-30 09:37:43 -0300

    final

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 $(function(){
     $("[data-toggle='tooltip']").tooltip();
-  });
-  $('.carousel').carousel({
-    interval: 2000
-  })
+
+    $('.carousel').carousel({
+        interval: 2000
+    },
+
+    $('#myModal').on('show.bs.modal', function(e){
+        $('myModal').removeClass('btn.info');
+        $('#myModal').addClass('btn-primary');
+        $('#myModal').prop('disabled', true);
+    }),
+
+    $('#myModal').on('shown.bs.modal', function(e){
+        console.log('el modal se mostro');
+    }),
+
+    $('#myModal').on('hide.bs.modal', function(e){
+        console.log('el modal esta escondiendo');
+    }),
+
+    $('#myModal').on('hidden.bs.modal', function(e){
+        console.log('el modal se escondio');
+    })
+})
