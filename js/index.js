1d211c9b43ad05447cc2d2381e44b20e87954e72
juanMelgratti <juanmelgratti@mi.unc.edu.ar>
2020-09-30 10:01:48 -0300

    final

--- js/index.js
+++ js/index.js
@@ -3,23 +3,23 @@ $(function(){
 
     $('.carousel').carousel({
         interval: 2000
-    },
+    });
+
 
     $('#myModal').on('show.bs.modal', function(e){
-        $('myModal').removeClass('btn.info');
-        $('#myModal').addClass('btn-primary');
         $('#myModal').prop('disabled', true);
-    }),
+    });
 
     $('#myModal').on('shown.bs.modal', function(e){
         console.log('el modal se mostro');
-    }),
+    });
 
     $('#myModal').on('hide.bs.modal', function(e){
         console.log('el modal esta escondiendo');
-    }),
+    });
 
     $('#myModal').on('hidden.bs.modal', function(e){
         console.log('el modal se escondio');
-    })
+    });
+
 })
